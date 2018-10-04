4975d5cf54e38d4522d8056c19ceb791ea4a01be
Mohsen <smohsenmg7@gmail.com>
2018-10-04 10:31:48 +0330

    Add BottomNavigationView

---
+++ app/src/main/java/com/example/mohsen/salamatbashid/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.mohsen.salamatbashid;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_checkup:
+                    mTextMessage.setText(R.string.title_check_up);
+                    return true;
+                case R.id.navigation_promotion:
+                    mTextMessage.setText(R.string.title_offer);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.tx_message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
