b25b87f1754eea8206ba2068280109926b579ffb
Mohsen <smohsenmg7@gmail.com>
2018-10-04 23:21:21 +0330

    Add BottomNavigationView Listener -Add Three Fragment to handle BottomNavigationView Listener

--- app/src/main/java/com/example/mohsen/salamatbashid/MainActivity.java
+++ app/src/main/java/com/example/mohsen/salamatbashid/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.mohsen.salamatbashid;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private CheckUpFragment checkUpFragment;
+    private HomeFragment homeFragment;
+    private OfferFragment offerFragment;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,25 +23,35 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    setFragment(homeFragment);
                     return true;
                 case R.id.navigation_checkup:
-                    mTextMessage.setText(R.string.title_check_up);
+                    setFragment(checkUpFragment);
                     return true;
                 case R.id.navigation_promotion:
-                    mTextMessage.setText(R.string.title_offer);
+                    setFragment(offerFragment);
                     return true;
             }
             return false;
         }
     };
 
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment).commit();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.tx_message);
+        homeFragment = new HomeFragment();
+        checkUpFragment = new CheckUpFragment();
+        offerFragment = new OfferFragment();
+        setFragment(homeFragment);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
